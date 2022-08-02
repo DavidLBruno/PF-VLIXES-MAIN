76b8d2f6d25842573fee6ba68bdfa0225bf3c69c
DavidLBruno <bruno.david9914@gmail.com>
2022-08-02 10:13:26 -0300

    No heroku

--- api/src/routes/pay/index.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const { checkRole } = require("../../helpers/auth"); //garantiza una secion iniciada
-const { checkRules } = require("../../helpers/Token");
-
-const {postPay} = require("./functions")
-router.post("",checkRole,checkRules(["user", "admin"]),postPay)
-
-module.exports = {pay: router}
